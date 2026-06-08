23ce1daa51372d327bcd644bec9cf1c9162eee28
Zenith <zenithjdozucwuxgwn@gmail.com>
2026-06-08 00:30:36 -0400

    Update zgg.js

--- zgg.js
+++ zgg.js
@@ -1,5 +1,5 @@
 const ZGG = {
-    BASE_URL: "https://cdn.jsdelivr.net/gh/Zenith-UB/ZGG.js@main/",
+    BASE_URL: "https://cdn.jsdelivr.net/gh/Zenith-UB/ZGG.js@latest/",
     JSON_FILE: "g.json",
 
     async init(gridSelector, cardsAcross = 5) {
